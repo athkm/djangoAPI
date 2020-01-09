bd300aa17593f3d5f22063f2ba8fe50ebd553460
athkm <mrathkm@gmail.com>
2020-01-09 23:17:18 +0530

    O2O M2O M2M relationshps added

--- assignment/product/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Product(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.TextField(max_length=200)
-    price = models.DecimalField(decimal_places=2, max_digits=200, null=False)
